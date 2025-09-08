8f1c090257ddeb4ff629a885119f82b3a906fafe
huseyinkrs <121403058+HK27027@users.noreply.github.com>
2025-09-09 00:04:53 +0300

    cors hatası ve diğer düzenlemeler

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,6 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
+  app.enableCors({
+    origin: true, // Tüm origin'lere izin veriyorum
+    credentials: true,
+  });
+  
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
