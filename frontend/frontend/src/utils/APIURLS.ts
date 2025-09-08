9aba57c3778d6fae4ba32f1d197ff3f1e5fab6b9
huseyinkrs <121403058+HK27027@users.noreply.github.com>
2025-09-08 20:02:44 +0300

    Update APIURLS.ts

--- frontend/frontend/src/utils/APIURLS.ts
+++ frontend/frontend/src/utils/APIURLS.ts
@@ -1,5 +1,5 @@
 export class APIURLS {
-  private static readonly BASE_URL = 'http://localhost:3000';//normalde .env den alacaktım ama basit bir proje olduğu için almadım
+  private static readonly BASE_URL = 'http://localhost:3000';//normalde .env den alacaktım ama basit bir proje olduğu için almadım.
 
   public static readonly USERS = `${APIURLS.BASE_URL}/users`;
   public static readonly POSTS = `${APIURLS.BASE_URL}/posts`;
